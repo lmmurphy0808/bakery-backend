7bcecd681d05386a5548a067d39090b8a05393c5
Lauren Murphy <lmmurphy0808@users.noreply.github.com>
2025-04-28 00:26:07 -0400

    connected to mongo

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.static("public"));
 app.use("/uploads", express.static("uploads"));
 app.use(express.static("json"));
 app.use(cors());
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -19,9 +20,107 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+mongoose
+.connect(
+    "mongodb+srv://lmmurphy04:mduzf5dhqyqctgo2@cluster0.v3tf38q.mongodb.net/"
+)
+.then(() => {
+    console.log("connected to mongoDB");
+})
+.catch((error) => {
+    console.log("could not connect to mongoDB", error);
+});
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
+
+const cookieSchema = new mongoose.Schema({
+    name: String,
+    level: String,
+    rating: Number,
+    main_image: String
+});
+
+const House = mongoose.model("Cookie", cookieSchema);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/api/cookies/", async (req, res) => {
+    const cookies = await Cookie.find();
+    res.send(cookies);
+});
+
+app.get("/api/cookies/:id", async (req, res) => {
+    const cookie = await Cookie.findOne({_id: id });
+    res.send(cookie);
+});
+
+app.post("/api/cookies", upload.single("img"), async (req, res) => {
+    const result = validateCookie(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+    const cookie = new Cookie({
+        name: req.body.name,
+        level: req.body.level,
+        rating: req.body.rating,
+        main_image: req.body.main_image
+    });
+
+    if (req.file) {
+        cookie.main_image = "images/" + req.file.filename;
+    }
+
+    const newCookie = await cookie.save();
+    res.send(newCookie);
+});
+
+app.put ("/api/cookies/:id", upload.single("img"), async (req, res) => {
+    const result = validateCookie(req.body);
+
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    }
+
+        let fieldsToUpdate = {
+            name: req.body.name,
+            level: req.body.level,
+            rating: req.body.rating,
+            main_image: req.body.main_image
+        };
+    if (req.file) {
+        fieldsToUpdate.img = "images/" + req.file.filename;
+    }
+
+    const wentThrough = await Cookie.updateOne(
+        { _id: req.params.id },
+        fieldsToUpdate
+    );
+
+    const updatedCookie = await Cookie.findOne({ _id: req.params.id });
+    res.send(updatedCookie);
+});
+
+app.delete("/api/cookies/:id", async (req, res) => {
+    const cookie = await Cookie.findByIdAndDelete(req.params.id);
+    res.send(cookie);
+});
+
+const validateCookie = (cookie) => {
+    const schema = Joi.object({
+        name: Joi.string().min(3).max(30).required(),
+        level: Joi.string().min(3).max(30).required(),
+        rating: Joi.string().min(1).max(10).required(),
+        image: Joi.string().min(3).max(30).required(),
+    });
+    return schema.validate(cookie);
+};
 
 app.get("/api/cookies", (req, res) => {
 const cookies = [
@@ -60,10 +159,6 @@ const cookies = [
     //res.send(cookies);
 });
 
-app.get("/api/cookies/", (req, res) => {
-    res.send(cookies);
-});
-
 app.post("/api/cookies", upload.single("img"), (req, res) => {
     const result = validateCookie(req.body);
 
@@ -86,16 +181,6 @@ app.post("/api/cookies", upload.single("img"), (req, res) => {
     cookies.push(cookie);
     res.status(200).send(cookie);
 });
-
-const validateCookie = (cookie) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).max(30).required(),
-        level: Joi.string().min(3).max(30).required(),
-        rating: Joi.string().min(1).max(10).required(),
-    });
-    return schema.validate(cookie);
-
-}
 
 app.listen(3002, () => {
     console.log("I'm listening");
